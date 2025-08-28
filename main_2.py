df04b87d91ede4e46532e473c8f6a776e90e51a9
delmag138 <157709702+delmag138@users.noreply.github.com>
2025-08-28 23:40:30 +0300

    Create main_2.py

---
+++ main_2.py
@@ -0,0 +1,15 @@
+"""
+Ask the user for a word.
+
+Print "Yes, it’s a palindrome" if the word reads the same forwards and backwards.
+
+Example: madam → palindrome.
+"""
+
+def palindrome(user_input):
+    if user_input == user_input[::-1]:
+        print("Hurray, it's a palindrome word!")
+    else:
+        print("Nope, better luck next time!")
+
+palindrome(input("Enter a palindrome word:"))
